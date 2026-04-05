e176f3eeae423f4c4527d0a65b6666716f7441f0
buettees <buettees@gmail.com>
2026-04-05 18:40:36 +0600

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'BUET Tees <noreply@send.buettees.shop>',
+        from: 'BUET Tees <noreply@buettees.shop>',
         to: toArr,
         subject,
         html

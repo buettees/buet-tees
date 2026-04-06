9224df23729b3b9de9d10305d441a042cef73877
buettees <buettees@gmail.com>
2026-04-06 17:38:31 +0600

    Update upload-image.js

--- api/upload-image.js
+++ api/upload-image.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   try {
     const supabase = createClient(
       process.env.SUPABASE_URL,
-      process.env.SUPABASE_SERVICE_KEY
+      process.env.SUPABASE_SECRET_KEY
     );
 
     // Read raw body
